cd56c19c4f1e54a940aa9990a43855c0f076b1c4
MatiasReyes1925 <mat.reyesa@duocuc.cl>
2026-05-07 23:00:11 -0400

    Creación de módulo de Bitácoras de asignatura (Model Repository, Service y Controller)

--- micro_academico/src/main/java/com/example/micro_academico/ObjetivoDeAprendizaje/service/ObjetivoAprendizajeService.java
+++ micro_academico/src/main/java/com/example/micro_academico/ObjetivoDeAprendizaje/service/ObjetivoAprendizajeService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+// Servicio para gestionar los objetivos de aprendizaje
+
 @Service
 public class ObjetivoAprendizajeService {
 
@@ -50,3 +52,4 @@ public class ObjetivoAprendizajeService {
         repository.deleteById(id);
     }
 }
+
